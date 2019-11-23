ad4d0d2aadc79cd89a5d331aff03857213736a50
PeanutButte7 <adam.barta007@gmail.com>
2019-11-23 12:09:02 +0100

    updated dependencies, added bootstrap

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Adam Bárta</h1>
+    <p>hire me or i hire ur mother</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+
   }
 }
 </script>

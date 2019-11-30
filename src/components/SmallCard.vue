5563230f065d6810bf432eb466a7a3548dfb2128
PeanutButte7 <adam.barta007@gmail.com>
2019-11-30 10:52:29 +0100

    WIP SmallCard

---
+++ src/components/SmallCard.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="smallCard border border-light">
+        <img :src="" alt="Image">
+        <h1>{{ headline }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'smallCard',
+        props: {
+            headline: {
+                required: true,
+                type: String
+            },
+            imgSource: {
+                required: true,
+                type: String
+            }
+        },
+        data() {
+            return {
+                image: this.imgSource
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .smallCard {
+        height: 500px;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="home">
-    <h1>Adam Bárta</h1>
-    <p>hire me or i hire ur mother</p>
+  <div class="home mt-5 ml-5 mr-5">
+    <b-container>
+      <b-row>
+        <b-col><SmallCard headline="GPFC" img-source="../assets/SmallCard_GPFC.png"></SmallCard></b-col>
+        <b-col><SmallCard headline="GPFC" img-source="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F78.media.tumblr.com%2F10776e624890ff1b69653e6a0b649b1e%2Ftumblr_inline_ortrrhjE1f1qzf6t4_540.png&f=1&nofb=1"></SmallCard></b-col>
+        <b-col><SmallCard headline="GPFC" img-source="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F78.media.tumblr.com%2F10776e624890ff1b69653e6a0b649b1e%2Ftumblr_inline_ortrrhjE1f1qzf6t4_540.png&f=1&nofb=1"></SmallCard></b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import SmallCard from "../components/SmallCard";
 
 export default {
   name: 'home',
   components: {
-
+    SmallCard
   }
 }
 </script>
+
+

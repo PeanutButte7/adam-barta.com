a5806ab49bbdc604d15561eb95fddeaf82da9495
PeanutButte7 <adam.barta007@gmail.com>
2020-09-29 13:24:35 +0200

    HeadshotRound fix

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
     <div id="app" class="container mx-auto mb-10">
         <nav class="flex items-center mt-10 mb-0 lg:mt-20 lg:mb-6">
             <div class="flex items-center flex-shrink-0 mr-6">
-                <img src="./assets/HeadshotRound.png" alt="My photo" class="opacity-100 h-12">
+                <img src="./assets/HeadshotRound.jpf" alt="My photo" class="opacity-100 h-12">
                 <router-link to="/" class="text-3xl font-semibold leading-tight text-primary ml-4">Adam Barta</router-link>
             </div>
             <div class="flex flex-grow md:hidden">
                 <div class="flex-grow">
 
                 </div>
-                <button @click="menuActive = !menuActive" class="flex items-center">
+                <button type="button" @click="menuActive = !menuActive" class="flex items-center">
                     <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="stroke-current text-primary">
                         <path d="M3 12H21" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                         <path d="M3 6H21" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
